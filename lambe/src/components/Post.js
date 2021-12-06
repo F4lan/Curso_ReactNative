c47ac43a231606db5ec336d317fbbf5af159271e
Felipe Furlan <felipefurlan0@gmail.com>
2021-12-06 16:10:37 -0300

    iniciando lambe

---
+++ lambe/src/components/Post.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+    StyleSheet,
+    View,
+    Image,
+    Dimensions,
+    Text
+} from 'react-native'
+
+import Author from "./Author";
+import Comments from "./Comments";
+import AddComent from "./AddComment"
+
+class Post extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={this.props.image}
+                    style={styles.image}
+                />
+                <Author
+                    email='fulano@gmail.com'
+                    nickname='teste'
+                />
+                <Comments
+                    comments={this.props.comments}
+                />
+                <AddComent />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain'
+    },
+})
+
+export default Post
